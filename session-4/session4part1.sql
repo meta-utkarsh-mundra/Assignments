d50d9aaef6866dc73efda9efb974807eb466dcb8
meta-utkarsh-mundra <41181598+meta-utkarsh-mundra@users.noreply.github.com>
2018-08-21 16:49:18 +0530

    Update session4part1.sql

--- session-4/session4part1.sql
+++ session-4/session4part1.sql
@@ -10,3 +10,15 @@ create function number_of_orders (month int, year int)
     return(total_count);
  END
  $$
+ 
+  Delimiter $$
+create function number_of_orders (year int)
+	Returns int
+    begin
+    declare max_orders int;
+	select count(order_id) as count, month(order_date) into max_orders from orders
+		where year(order_id) = year
+		group by month(order_date) order by count desc limit 1;
+    return(max_orders);
+END
+$$    
